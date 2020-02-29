c515a8ac8e77ee2d1b2c886604dbac7ec6e8d73b
tssj <thomas.starup@gmail.com>
2020-02-29 21:29:05 +0100

    Second commit

---
+++ Bomberman/GameContent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace Bomberman
+{
+    public class GameContent
+    {
+        public ContentManager content { private set; get; }
+
+        public GameContent(ContentManager content)
+        {
+            this.content = content;
+        }
+    }
+}
---
+++ Bomberman/GameWorld.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomberman
+{
+    public class GameWorld
+    {
+
+        Game1 game1;
+        public GameTime gameTime;
+        SpriteBatch spriteBatch;
+        GameContent content;
+        KeyboardState keyState;
+
+        public static int screenWidth { private set; get; }
+        public static int screenHeight { private set; get; }
+
+        public static Random random;
+
+        public GameWorld(Game1 game1, SpriteBatch spriteBatch, GameContent content)
+        {
+            this.game1 = game1;
+            this.spriteBatch = spriteBatch;
+            this.content = content;
+
+            screenWidth = game1.GraphicsDevice.Viewport.Width;
+            screenHeight = game1.GraphicsDevice.Viewport.Height;
+        }
+    }
+}
