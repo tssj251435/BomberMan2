e0117491eebae0d162c759e19c34242ce84ca865
karl mogensen <karlmogensen@hotmail.com>
2020-03-01 11:39:44 +0100

    player class

--- Bomberman/GraphicalElements/Player.cs
+++ Bomberman/GraphicalElements/Player.cs
@@ -8,10 +8,17 @@ using System.Threading.Tasks;
 
 namespace Bomberman.GraphicalElements
 {
+    public enum Direction { Right, Left, Up, Down}
+
     class Player : GraphicalElement
     {
 
         public Player(SpriteBatch spriteBatch, Texture2D image, Vector2 position) : base(spriteBatch, image, position) { }
+
+        List<Texture2D> RightAnimations;
+        List<Texture2D> LeftAnimations;
+
+
 
         public override void Draw()
         {
@@ -24,6 +31,30 @@ namespace Bomberman.GraphicalElements
                 1f,
                 SpriteEffects.None,
                 0f);
+        }
+
+        public bool IsMoving;
+        public Direction Direction;
+        private Direction previousDirection;
+        private int animationSelecter;
+        public void Move()
+        { 
+            if (previousDirection != Direction)
+                animationSelecter = 0;
+
+            switch (Direction)
+            {
+                case Direction.Right:
+                    break;
+                case Direction.Left:
+                    break;
+                case Direction.Up:
+                    break;
+                case Direction.Down:
+                    break;
+            }
+
+            previousDirection = Direction;
         }
 
     }
